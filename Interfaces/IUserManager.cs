e9b65f1b1c7905915a796dbebbf258e74c86fcbb
poojadonawadi <138244294+poojadonawadi@users.noreply.github.com>
2025-01-29 20:36:30 +0530

    [Pooja] added: ManagerLayer Interface

---
+++ Interfaces/IUserManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CommonLayer.Models;
+using RepositoryLayer.Entity;
+
+namespace ManagerLayer.Interfaces
+{
+    public interface IUserManager
+    {
+        public Users Registration(RegisterModel model);
+
+        public string Login(LoginModel login);
+
+        public bool MailExist(string email);
+
+        public ForgotPasswordModel ForgetPassword(string email);
+    }
+}
