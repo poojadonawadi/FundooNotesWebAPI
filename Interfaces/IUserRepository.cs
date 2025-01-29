d4e3caf05a61208f768b68d046a2864abce9cdb5
poojadonawadi <138244294+poojadonawadi@users.noreply.github.com>
2025-01-29 21:33:39 +0530

    [Pooja] added: RepositoryLayer Interfaces

---
+++ Interfaces/IUserRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CommonLayer.Models;
+using RepositoryLayer.Entity;
+using RepositoryLayer.Migrations;
+
+namespace RepositoryLayer.Interfaces
+{
+    public interface IUserRepository
+    {
+        public Users Registration(RegisterModel model);
+
+        public string Login(LoginModel login);
+
+        public ForgotPasswordModel ForgetPassword(string email);
+    }
+}
